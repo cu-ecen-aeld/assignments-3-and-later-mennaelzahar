23846a97f2718cecf4dd5470d69f0289a1fb34db
mennaelzahar <menna.elzahar@smarteye.ai>
2025-06-14 19:00:34 +0300

    Build Kernel

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -13,12 +13,11 @@ FINDER_APP_DIR=$(realpath $(dirname $0))
 ARCH=arm64
 CROSS_COMPILE=aarch64-none-linux-gnu-
 
-if [ $# -lt 1 ]
-then
-	echo "Using default directory ${OUTDIR} for output"
+if [ $# -lt 1 ]; then
+    echo "Using default directory ${OUTDIR} for output"
 else
-	OUTDIR=$1
-	echo "Using passed directory ${OUTDIR} for output"
+    OUTDIR=$1
+    echo "Using passed directory ${OUTDIR} for output"
 fi
 
 mkdir -p ${OUTDIR}
@@ -26,8 +25,8 @@ mkdir -p ${OUTDIR}
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/linux-stable" ]; then
     #Clone only if the repository does not exist.
-	echo "CLONING GIT LINUX STABLE VERSION ${KERNEL_VERSION} IN ${OUTDIR}"
-	git clone ${KERNEL_REPO} --depth 1 --single-branch --branch ${KERNEL_VERSION}
+    echo "CLONING GIT LINUX STABLE VERSION ${KERNEL_VERSION} IN ${OUTDIR}"
+    git clone ${KERNEL_REPO} --depth 1 --single-branch --branch ${KERNEL_VERSION}
 fi
 if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     cd linux-stable
@@ -35,46 +34,127 @@ if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     git checkout ${KERNEL_VERSION}
 
     # TODO: Add your kernel build steps here
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} mrproper
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} defconfig
+    make -j4 ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} all
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} modules
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} dtbs
 fi
 
 echo "Adding the Image in outdir"
+cp ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ${OUTDIR}
 
 echo "Creating the staging directory for the root filesystem"
 cd "$OUTDIR"
-if [ -d "${OUTDIR}/rootfs" ]
-then
-	echo "Deleting rootfs directory at ${OUTDIR}/rootfs and starting over"
-    sudo rm  -rf ${OUTDIR}/rootfs
+if [ -d "${OUTDIR}/rootfs" ]; then
+    echo "Deleting rootfs directory at ${OUTDIR}/rootfs and starting over"
+    sudo rm -rf ${OUTDIR}/rootfs
 fi
 
 # TODO: Create necessary base directories
+mkdir -p ${OUTDIR}/rootfs
+cd ${OUTDIR}/rootfs
+mkdir -p bin dev etc home lib lib64 proc sbin sys tmp usr var
+mkdir -p usr/bin usr/lib usr/sbin
+mkdir -p var/log
 
 cd "$OUTDIR"
-if [ ! -d "${OUTDIR}/busybox" ]
-then
-git clone git://busybox.net/busybox.git
+if [ ! -d "${OUTDIR}/busybox" ]; then
+    git clone git://busybox.net/busybox.git
     cd busybox
     git checkout ${BUSYBOX_VERSION}
     # TODO:  Configure busybox
+    make distclean
+    make defconfig
 else
     cd busybox
 fi
 
 # TODO: Make and install busybox
+make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
+make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} CONFIG_PREFIX=${OUTDIR}/rootfs install
 
 echo "Library dependencies"
-${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
-${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
+${CROSS_COMPILE}readelf -a ${OUTDIR}/rootfs/bin/busybox | grep "program interpreter"
+${CROSS_COMPILE}readelf -a ${OUTDIR}/rootfs/bin/busybox | grep "Shared library"
 
 # TODO: Add library dependencies to rootfs
+SYSROOT=$(${CROSS_COMPILE}gcc --print-sysroot)
+echo "My SYSROOT is -----------> $SYSROOT"
+
+echo "Libraries in SYSROOT/lib64/ are:---------->"
+ls -l ${SYSROOT}/lib64/
+
+# Copy dynamic linker (special path)
+cp -v "${SYSROOT}/lib/ld-linux-aarch64.so.1" "${OUTDIR}/rootfs/lib/" || {
+    echo "ERROR: Failed to copy ld-linux-aarch64.so.1"
+    exit 1
+}
+
+# Copy all REQUIRED libraries and their symlinks
+for lib in libc libm libresolv; do
+    # Copy the .so file (e.g., libresolv.so.2)
+    if ls "${SYSROOT}/lib64/${lib}.so.6" >/dev/null 2>&1; then
+        cp -v "${SYSROOT}/lib64/${lib}.so.6" "${OUTDIR}/rootfs/lib64/" || {
+            echo "ERROR: Failed to copy ${lib}.so.6"
+            exit 1
+        }
+    fi
+
+    # Copy the versioned file if it exists (e.g., libresolv-2.33.so)
+    if ls "${SYSROOT}/lib64/${lib}-"*.so >/dev/null 2>&1; then
+        cp -v ${SYSROOT}/lib64/${lib}-*.so "${OUTDIR}/rootfs/lib64/"
+    fi
+done
+
+# Special case for libresolv.so.2 (which uses .so.2 suffix instead of .so.6)
+cp -v "${SYSROOT}/lib64/libresolv.so.2" "${OUTDIR}/rootfs/lib64/" || {
+    echo "ERROR: Failed to copy libresolv.so.2"
+    exit 1
+}
+
+# 4. Verify all critical files exist
+required_files=(
+    "lib/ld-linux-aarch64.so.1"
+    "lib64/libc.so.6"
+    "lib64/libm.so.6"
+    "lib64/libresolv.so.2" # Explicitly verified
+)
+
+for file in "${required_files[@]}"; do
+    if [ ! -e "${OUTDIR}/rootfs/${file}" ]; then
+        echo "ERROR: Missing required file ${file}"
+        exit 1
+    fi
+done
+
+echo "All required libraries copied successfully!"
+ls -l ${OUTDIR}/rootfs/lib/ld-linux* ${OUTDIR}/rootfs/lib64/lib{c,m,resolv}*
 
 # TODO: Make device nodes
+sudo mknod -m 666 ${OUTDIR}/rootfs/dev/null c 1 3
+sudo mknod -m 600 ${OUTDIR}/rootfs/dev/console c 5 1
 
 # TODO: Clean and build the writer utility
+cd ${FINDER_APP_DIR}
+make clean
+make CROSS_COMPILE=${CROSS_COMPILE}
 
 # TODO: Copy the finder related scripts and executables to the /home directory
 # on the target rootfs
+cp writer ${OUTDIR}/rootfs/home
+cp finder.sh ${OUTDIR}/rootfs/home
+cp finder-test.sh ${OUTDIR}/rootfs/home
+cp autorun-qemu.sh ${OUTDIR}/rootfs/home
+mkdir -p ${OUTDIR}/rootfs/home/conf
+cp conf/username.txt ${OUTDIR}/rootfs/home/conf
+cp conf/assignment.txt ${OUTDIR}/rootfs/home/conf
+sed -i 's|\.\./conf/assignment.txt|conf/assignment.txt|g' ${OUTDIR}/rootfs/home/finder-test.sh
 
 # TODO: Chown the root directory
+cd ${OUTDIR}/rootfs
+sudo chown -R root:root *
 
 # TODO: Create initramfs.cpio.gz
+find . | cpio -H newc -ov --owner root:root >${OUTDIR}/initramfs.cpio
+gzip -f ${OUTDIR}/initramfs.cpio
